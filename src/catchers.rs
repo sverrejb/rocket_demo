acd2996a0fd1e9c4e2439ed1ae926ba7a392a13b
Sverre Johann Bjørke <sverre.bjorke@knowit.no>
2022-09-22 23:27:51 +0200

    utkommentert ting

--- src/catchers.rs
+++ src/catchers.rs
@@ -1,13 +1,13 @@
 use rocket::Request;
 
-#[catch(404)]
-pub fn not_found(req: &Request) -> String {
-    format!("Vi fant ingenting på '{}'. Prøv noe annet?", req.uri())
-}
-
 #[catch(400)]
 pub fn bad_request(req: &Request) -> String {
     //Stygg hack
     let error = req.local_cache(|| "");
     format!("400, bad request: {error}")
+}
+
+#[catch(404)]
+pub fn not_found(req: &Request) -> String {
+    format!("Vi fant ingenting på '{}'. Prøv noe annet?", req.uri())
 }
--- src/main.rs
+++ src/main.rs
@@ -9,23 +9,22 @@ mod templating;
 
 #[get("/<name>/<age>")]
 fn hello_age(name: &str, age: u8) -> String {
-    format!("{}, du er {} år gammel!", age, name)
+    format!("{}, du er {} år gammel!", name, age)
 }
 
 #[get("/<name>/<nickname>", rank = 2)]
 fn hello_nick(name: &str, nickname: &str) -> String {
-    format!("Hallo, {} AKA {}!", name, nickname)
+    format!("Hallo, {name} AKA {nickname}!")
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build()
-        .mount("/hello", routes![hello_age, hello_nick])
-        .register("/", catchers![catchers::not_found, catchers::bad_request])
-        .mount("/math", routes![math::add])
-        .mount("/public", rocket::fs::FileServer::from("static/"))
-        .mount("/sensitive", routes![secrets::sensitive])
-        .mount("/json", routes![json::todo, json::new_todo])
-        .attach(Template::fairing())
-        .mount("/template", routes![templating::hello])
+    rocket::build().mount("/hello", routes![hello_age, hello_nick])
+    //.register("/", catchers![catchers::not_found, catchers::bad_request])
+    //.mount("/math", routes![math::add])
+    //.mount("/public", rocket::fs::FileServer::from("static/"))
+    //.mount("/sensitive", routes![secrets::sensitive])
+    //.mount("/json", routes![json::todo, json::new_todo])
+    //.attach(Template::fairing())
+    //.mount("/template", routes![templating::hello])
 }
